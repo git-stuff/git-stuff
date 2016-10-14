af592405af6d4dd2d10a585cd678124958733a2d
Alf Gaida <agaida@siduction.org>
2016-10-14 02:05:07 +0200

    Adding upstream version 15.
    Signed-off-by: Alf Gaida <agaida@siduction.org>

--- manpages/bin/update-version.sh
+++ manpages/bin/update-version.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-DATE="$(LC_ALL=C date +%Y\\\\-%m\\\\-%d)"
 PROGRAM="GIT\\\-STUFF"
 VERSION="$(cat ../VERSION)"
+
+DATE="$(LC_ALL=C date +%Y\\\\-%m\\\\-%d)"
+
+DAY="$(LC_ALL=C date +%d)"
+MONTH="$(LC_ALL=C date +%m)"
+YEAR="$(LC_ALL=C date +%Y)"
 
 echo "Updating version headers..."
 
@@ -13,4 +18,18 @@ do
 	SECTION="$(basename ${MANPAGE} | awk -F. '{ print $2 }')"
 
 	sed -i -e "s|^.TH.*$|.TH ${PROGRAM} ${SECTION} ${DATE} ${VERSION} \"Git Stuff\"|" ${MANPAGE}
+done
+
+# European date format
+for _LANGUAGE in de
+do
+	if ls po/${_LANGUAGE}/*.po > /dev/null 2>&1
+	then
+		for _FILE in po/${_LANGUAGE}/*.po
+		do
+			sed -i  -e "s|^msgstr .*.2013-.*$|msgstr \"${DAY}.${MONTH}.${YEAR}\"|g" \
+				-e "s|^msgstr .*.2013\"$|msgstr \"${DAY}.${MONTH}.${YEAR}\"|g" \
+			"${_FILE}"
+		done
+	fi
 done
